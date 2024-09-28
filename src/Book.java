d93089217b36c650a060e045c7c679a291e8fc53
Dumer <dumeti@gmail.com>
2024-09-28 15:17:55 +0500

    Add code on serch problem

--- src/Book.java
+++ src/Book.java
@@ -3,4 +3,32 @@ public class Book {
     private String author;
     private int year;
 
+    public Book() {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        Author Nikolay = new Author("Николай", "Гоголь");
+        Book DeadSoul = new Book("Мертвые души","Nikolay",1835);
+        Author Alex = new Author("Александер", "Пушкин");
+        Book Dubrovski = new Book("Дубровский", "Alex",1941);
+        System.out.println("Имя автора: " + Nikolay.getName());
+        System.out.println("Фамилия автора: " + Nikolay.getLastname());
+        System.out.println("Название книги: " + DeadSoul.getTitle());
+        System.out.println("Автор книги: " + DeadSoul.getAuthor());
+        System.out.println("Год книги: " + DeadSoul.getYear());
+        System.out.println("Имя автора: " + Alex.getName());
+        System.out.println("Фамилия автора: " + Alex.getLastname());
+        System.out.println("Название книги: " + Dubrovski.getTitle());
+        System.out.println("Автор книги: " + Dubrovski.getAuthor());
+        System.out.println("Год книги: " + Dubrovski.getYear());
+        Dubrovski.setYear(1940);
+        System.out.println("Исправление в годе книги: " + Dubrovski.getYear());
 
 
 
